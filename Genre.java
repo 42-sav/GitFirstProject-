7b21a14334898e3230dce175cc0b8e7bcfa8103c
42-sav <84896130+42-sav@users.noreply.github.com>
2022-11-03 01:11:28 +0530

    Add files via upload

---
+++ Genre.java
@@ -0,0 +1,13 @@
+public class Genre {
+
+    public void list() {
+        System.out.println("Thriller");
+        System.out.println("Comic");
+        System.out.println("Drama");
+        System.out.println("Mystery");
+        System.out.println("Crime");
+
+
+    }
+    
+}
